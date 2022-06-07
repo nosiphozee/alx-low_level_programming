665109ed616560ca9de18d8231263a3951bddec1
nosipho <nosiphonatacia@gmail.com>
2022-06-07 05:30:18 -0700

    putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,22 +1,19 @@
 #include "main.h"
-
 /**
- * main - prints string "_putchar"
+ * main - Entry point
  *
- *
- * Return: Always return 0
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
-		_putchar('H');
-		_putchar('o');
-		_putchar('l');
-		_putchar('b');
-		_putchar('e');
-		_putchar('r');
-		_putchar('t');
-		_putchar('o');
-		_putchar('n');
-		_putchar('\n');
-return (0);
+	char *sh = "main";
+
+	while (*sh)
+	{
+		_putchar(*sh);
+		sh++;
+	}
+	_putchar('\n');
+
+	return (0);
 }
